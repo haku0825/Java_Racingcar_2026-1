c30b887472b092b2e691737f0c6e9473b1400b06
songzio <zioziozzang@naver.com>
2026-03-25 18:34:41 +0900

    RacingGame안에 for위치 변경, application작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        // 1. 게임 엔진 객체 생성
+        RacingGame racingGame = new RacingGame();
 
+        // 2. 게임 실행 및 최종 자동차 리스트 받아오기
+        // RacingGame.java의 start 메서드는 List<Car>를 반환하도록 설계하셨습니다.
+        List<Car> carList = racingGame.start(null);
+
+        // 3. 우승자 판별 및 결과 출력
+        RacingResult racingResult = new RacingResult();
+        racingResult.result(carList);
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -13,23 +13,26 @@ public class RacingGame {
         List<Car> carList = inputView.createCars();
         int count = inputView.getCount();
 
+        System.out.println("실행결과");
+
         for (int i = 0; i < count; i ++) {
             for (Car car : carList) {
                 int randomNumber = Randoms.pickNumberInRange(0,9);
                 car.upCount(randomNumber);
             }
-        }
 
-        for (Car car : carList) {
-            int carCount = car.getCount();
-            String carName = car.getCarName();
+            for (Car car : carList) {
+                int carCount = car.getCount();
+                String carName = car.getCarName();
 
-            System.out.print(carName + " : ");
+                System.out.print(carName + " : ");
 
-            for (int j = 0; j < carCount; j++) {
-                System.out.print("-");
+                for (int j = 0; j < carCount; j++) {
+                    System.out.print("-");
+                }
+
+                System.out.println();
             }
-
             System.out.println();
         }
         return carList;
