c1e5565eb0ac619f5ac6a4cc2496c53d2c2dd2d2
songzio <zioziozzang@naver.com>
2026-03-25 15:57:24 +0900

    차량 이동 횟수 입력 기능 완료

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -10,14 +10,18 @@ public class InputView {
     public List<Car> createCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-        String input = Console.readLine();
-        String[] carNames = input.split(",");
+        String nameInput = Console.readLine();
+        String[] carNames = nameInput.split(",");
 
         List<String> existNames = new ArrayList<>(); //차 이름 중복 예외처리 하려고 만든 리스트
 
         List<Car> carList = new ArrayList<>();
 
         for (String name : carNames) {
+
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("차 이름 5글자보다 클 수 없음");
+            }
 
             if (name.isEmpty() || name.contains(" ")) {
                 throw new IllegalArgumentException("빈값, 공백 오류");
@@ -32,5 +36,25 @@ public class InputView {
             carList.add(car);
         }
         return carList;
+    }
+
+    public int getCount() {
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        String countInput = Console.readLine();
+
+        int tryCount;
+
+        try {
+            tryCount = Integer.parseInt(countInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야함");
+        }
+
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("0보다 작은 수 안됨");
+        }
+
+        return tryCount;
     }
 }
